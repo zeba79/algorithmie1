b067548f0af75dbc5aca589be3f35e7816897154
Antoine ZEBAZE <zeba_antoine@yahoo.fr>
2023-02-10 13:57:03 +0100

    modifications

--- Lecture_Ecriture/app.js
+++ Lecture_Ecriture/app.js
@@ -4,6 +4,7 @@ giveMe.addEventListener('click', function(){
     // let greetings = document.getElementById('greet');
     let username = prompt("Quel est notre nom ?");
     let age = prompt("Quel est ton age ?");
+ 
     let civilite = prompt("Votre civilité");
     
     document.querySelector("#greet").innerHTML = `Bonjour , ${civilite} ${username} , vous avez ${age} ans !`
